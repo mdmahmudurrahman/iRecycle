87a755ca58a53a62a50f5df8305d91d5dcac36b6
mdmahmudurrahman <mailtommrr@gmail.com>
2021-07-20 17:11:22 +0200

    user authentication

--- app/models/material.rb
+++ app/models/material.rb
@@ -4,6 +4,8 @@ class Material < ApplicationRecord
   has_many :recycle_center_materials, dependent: :destroy
   has_many :recycle_centers, through: :recycle_center_materials
 
+  enum status: [:shipped, :being_packaged, :complete, :cancelled]
+
   class << self
     def material_options materials
       materials.map{|material| {name: material.name, id: material.id }}
